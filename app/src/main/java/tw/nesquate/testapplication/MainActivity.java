6db3340296cb346bf2901cbd603b825347c7e3b4
Nesquate <nesquate.100001@gmail.com>
2023-04-01 18:45:16 +0800

    (Sample) find id

--- app/src/main/java/tw/nesquate/testapplication/MainActivity.java
+++ app/src/main/java/tw/nesquate/testapplication/MainActivity.java
@@ -2,13 +2,18 @@ package tw.nesquate.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(androidx.core.R.id.action_container);
     }
 }
